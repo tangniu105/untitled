b16b71b1923b80131b868ce92f691c172531a4f7
tangniu <ssblszzz@163.com>
2021-05-06 23:53:42 +0800

    111

---
+++ src/main/java/hello.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class hello {
+    public static void main(String[] args) {
+
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(1, 2);
+        map.put(2, 2);
+        map.put(3, 2);
+        map.put(4, 2);
+        map.put(5, 2);
+        System.out.println(map.get(5));
+    }
+}
