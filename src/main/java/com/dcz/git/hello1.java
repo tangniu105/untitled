10f0fcb61cbbec323beb663a83780777d1e63cdb
tangniu <tangniu@162.com>
2021-05-11 21:45:48 +0800

    hot-fix 111

--- src/main/java/com/dcz/git/hello1.java
+++ src/main/java/com/dcz/git/hello1.java
@@ -4,6 +4,6 @@ public class hello1 {
     public static void main(String[] args) {
         System.out.println("0000");
         System.out.println("1111");
-        System.out.println("2222");
+        System.out.println("4444");
     }
 }
