e9908fd68258817dd4145e6059b4c731e3e42682
tangniu <tangniu@162.com>
2021-05-15 16:48:18 +0800

    master111

--- src/main/java/com/dcz/git/hello.java
+++ src/main/java/com/dcz/git/hello.java
@@ -7,6 +7,7 @@ public class hello {
     public static void main(String[] args) {
 
         HashMap<Object, Object> map = new HashMap<>();
+        int a = 10 ;
         map.put(1, 2);
         map.put(2, 2);
         map.put(3, 2);
